e2197c33a385aeffc0f8e2c5e804ba883d727129
Alan <alan4tomaz8@gmail.com>
2025-05-29 23:34:56 -0300

    Start Creating Items List

--- client/src/app/components/pages/home/reusableComponents/items-list/items-list.component.ts
+++ client/src/app/components/pages/home/reusableComponents/items-list/items-list.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-items-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './items-list.component.html',
-  styleUrl: './items-list.component.scss'
+  styleUrl: './items-list.component.scss',
 })
 export class ItemsListComponent {
+  @ViewChild('exportDropdown') exportDropdown!: ElementRef;
 
+  isExportDropdownOpen: boolean = false;
+  isExportDropdownHovering: boolean = false;
+
+  handleShowExportDropdown(): void {
+    this.isExportDropdownOpen = !this.isExportDropdownOpen;
+  }
+
+  // Close the dropdowns when clicking outside of them
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    // Close the time filter dropdown if it is open and the click is outside of it
+    if (
+      this.isExportDropdownOpen &&
+      this.exportDropdown &&
+      !this.exportDropdown.nativeElement.contains(target) &&
+      !target.closest('.hover\\:cursor-pointer')
+    ) {
+      this.isExportDropdownOpen = false;
+    }
+  }
 }
